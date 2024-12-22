f03685163e40c3121d9ced05e4fa3c8986b0abe6
saheerkhan47 <saheerkhan4710@gmail.com>
2024-12-22 11:24:49 +0530

    Removed freelancerportal to prepare for splitting repositories

--- Freelance_Portal/Admin/acceptfree.php
+++
@@ -1,8 +0,0 @@
-<?php
-   include("../dboperation.php");
-   $obj=new dboperation();
-   $loginid=$_GET["loginid"];
-   echo $sql="update `tbllogin` set `status`='Confirm' where loginid='$loginid'";
-   $result=$obj->executequery($sql); 
-?>
-<script>window.location='freelancerview.php'</script>
